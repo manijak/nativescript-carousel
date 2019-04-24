82226f87629465f2d7d2071954c4567b8dcb8861
manijak <erkocevic@gmail.com>
2019-04-24 21:56:23 +0200

    TS demo app cleanup

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,2 +1,2 @@
 import * as application from 'tns-core-modules/application';
-application.start({ moduleName: 'main-page' });
+application.run({ moduleName: 'app-root' });
--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,11 +1,6 @@
 import { topmost } from 'tns-core-modules/ui/frame';
 
 export function pageLoaded() {
-  const image1 = 'https://raw.githubusercontent.com/NativeScript/sample-PhotoAlbum/master/app/res/05.jpg';
-  const image2 = 'https://raw.githubusercontent.com/NativeScript/sample-PhotoAlbum/master/app/res/06.jpg';
-  const image3 = 'https://raw.githubusercontent.com/NativeScript/sample-PhotoAlbum/master/app/res/07.jpg';
-  const image4 = 'https://raw.githubusercontent.com/NativeScript/sample-PhotoAlbum/master/app/res/08.jpg';
-  const myImages = [image1, image2, image3, image4];
 };
 
 export function carousel1() {
